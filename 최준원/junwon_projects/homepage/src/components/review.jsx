fda8d02eb433892672ad431ab0caca8705f15228
junwon1997 <junwon20170339@gmail.com>
2022-05-18 18:17:57 +0900

    react change

--- 최준원/junwon_projects/homepage/src/components/review.jsx
+++ 최준원/junwon_projects/homepage/src/components/review.jsx
@@ -15,13 +15,21 @@ import React, { Component } from "react";
 
 class Review extends Component {
   render() {
-    const { review } = this.props.review;
-    console.log(this.props);
     return (
       <div className="review">
-        <div className="review_box"></div>
+        {/* <div className="review_box"></div> */}
+        <a href={`https://www.youtube.com/watch?v=${this.props.review.id}`}>
+          <div class="review_box">
+            <img
+              class="review_img_box"
+              src={this.props.review.snippet.thumbnails?.high.url}
+            />
+          </div>
+        </a>
         <div className="review_box_title">UKOV 22기</div>
-        <div className="review_box_content">{review}</div>
+        <div className="review_box_content">
+          {this.props.review.snippet.title}
+        </div>
       </div>
     );
   }
--- 최준원/junwon_projects/homepage/src/components/reviews.jsx
+++ 최준원/junwon_projects/homepage/src/components/reviews.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Review from "./review";
+import axios from "axios";
 
 function Reviews() {
-  const reviews = [
-    {
-      review: "유일한 졸업생 단원이 전하는 후기 글이 넘치면 이렇게 줄여서 ...",
-    },
-    { review: "유코브 선배들이 알려주는 유코브 합격꿀팁!" },
-    { review: "유코브 선배들이 알려주는 유코브 합격꿀팁!" },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2C%20statistics&chart=mostPopular&maxResults=3&regionCode=kr&key=AIzaSyD-fIs7jzjFUiX4LV9uECojzUTwIUS1gQo"
+      )
+      .then((res) => setData(res.data.items));
+  }, []);
+
   return (
     <>
       <div className="reviews_title">UKOV 후기</div>
       <div className="reviews_mini_title">더 많은 후기 보러가기</div>
       <div className="reviews_all">
-        {reviews.map((review) => (
+        {data.map((review) => (
           <Review review={review} />
         ))}
       </div>
