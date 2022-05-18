569691d8c8eeb56c2d7a47824b01bb8e385f773e
junwon1997 <junwon20170339@gmail.com>
2022-05-18 20:01:27 +0900

    react change(05.18)

--- 최준원/junwon_projects/homepage/src/components/navbar.jsx
+++ 최준원/junwon_projects/homepage/src/components/navbar.jsx
@@ -4,13 +4,15 @@ import Ukov from "../assets/ukov.png";
 function Navbar() {
   return (
     <nav>
-      <div className="nav_logo">
-        <img src={Ukov} alt="Ukov" id="nav_logo" />
-      </div>
-      <div className="nav_button_list">
-        <span className="nav_button">홈</span>
-        <span className="nav_button">프로그램</span>
-        <span className="nav_button">리쿠르팅</span>
+      <div className="nav">
+        <div className="nav_logo">
+          <img src={Ukov} alt="Ukov" id="nav_logo" />
+        </div>
+        <div className="nav_button_list">
+          <span className="nav_button">홈</span>
+          <span className="nav_button">프로그램</span>
+          <span className="nav_button">리쿠르팅</span>
+        </div>
       </div>
     </nav>
   );
--- 최준원/junwon_projects/homepage/src/components/partner.jsx
+++ 최준원/junwon_projects/homepage/src/components/partner.jsx
@@ -1,38 +1,38 @@
 import React, { Component } from "react";
 
-// function Partner(props) {
-//   console.log(props);
-//   return (
-//     <div>
-//       <div className="remainder_partner">
-//         <img
-//           src={props.partner}
-//           className="remainder_partner_img"
-//           alt="담번에 보여줌~"
-//         />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Partner;
-
-class Partner extends Component {
-  render() {
-    const { partner } = this.props.partner;
-    console.log(this.props);
-    return (
-      <div>
-        <div className="remainder_partner">
-          <img
-            src={partner}
-            className="remainder_partner_img"
-            alt="see you next time"
-          />
-        </div>
+function Partner(props) {
+  console.log(props);
+  return (
+    <div>
+      <div className="remainder_partner">
+        <img
+          src={props.partner.partner}
+          className="remainder_partner_img"
+          alt="담번에 보여줌~"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Partner;
+
+// class Partner extends Component {
+//   render() {
+//     const { partner } = this.props.partner;
+//     console.log(this.props);
+//     return (
+//       <div>
+//         <div className="remainder_partner">
+//           <img
+//             src={partner}
+//             className="remainder_partner_img"
+//             alt="see you next time"
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Partner;
