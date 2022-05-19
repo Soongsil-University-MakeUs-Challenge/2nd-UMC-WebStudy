708e6fd1dd9504282554acb55dac476d3823920e
junwon1997 <junwon20170339@gmail.com>
2022-05-19 23:54:25 +0900

    react change(axios plus)

--- 최준원/junwon_projects/homepage/src/components/review.jsx
+++ 최준원/junwon_projects/homepage/src/components/review.jsx
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
 
-// function Review(props) {
-//   return (
-//     <div className="review">
-//       {/* <div className="review_box"></div> */}
-//       <a href={`https://www.youtube.com/watch?v=${props.review.id}`}>
-//         <div class="review_box">
-//           <img
-//             class="review_img_box"
-//             src={props.review.snippet.thumbnails?.high.url}
-//           />
-//         </div>
-//       </a>
-//       <div className="review_box_title">UKOV 22기</div>
-//       <div className="review_box_content">{props.review.snippet.title}</div>
-//     </div>
-//   );
-// }
-
-// export default Review;
-
 function Review(props) {
   return (
     <div className="review">
-      <div className="review_box"></div>
+      {/* <div className="review_box"></div> */}
+      <a href={`https://www.youtube.com/watch?v=${props.review.id}`}>
+        <div class="review_box">
+          <img
+            class="review_img_box"
+            src={props.review.snippet.thumbnails?.high.url}
+          />
+        </div>
+      </a>
       <div className="review_box_title">UKOV 22기</div>
-      <div className="review_box_content">{props.review}</div>
+      <div className="review_box_content">{props.review.snippet.title}</div>
     </div>
   );
 }
 
 export default Review;
+
+// function Review(props) {
+//   return (
+//     <div className="review">
+//       <div className="review_box"></div>
+//       <div className="review_box_title">UKOV 22기</div>
+//       <div className="review_box_content">{props.review.review}</div>
+//     </div>
+//   );
+// }
+
+// export default Review;
 
 // class Review extends Component {
 //   render() {
