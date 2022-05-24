c6dab9df49ceaf132d4aaf65684eac049477b0e5
junwon1997 <junwon20170339@gmail.com>
2022-05-25 01:03:12 +0900

    react routing

--- tommy/junwon_projects/homepage/src/components/navbar.jsx
+++ tommy/junwon_projects/homepage/src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Ukov from "../assets/ukov.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -9,9 +10,15 @@ function Navbar() {
           <img src={Ukov} alt="Ukov" id="nav_logo" />
         </div>
         <div className="nav_button_list">
-          <span className="nav_button">홈</span>
-          <span className="nav_button">프로그램</span>
-          <span className="nav_button">리쿠르팅</span>
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+            <span className="nav_button">소개</span>
+          </Link>
+          <Link to="/tommy" style={{ textDecoration: "none", color: "black" }}>
+            <span className="nav_button">후기</span>
+          </Link>
+          <Link to="umc_web" style={{ textDecoration: "none", color: "black" }}>
+            <span className="nav_button">프로그램 및 기타</span>
+          </Link>
         </div>
       </div>
     </nav>
---
+++ tommy/junwon_projects/homepage/src/components/tommy.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tommy() {
+  return <h1>My name is Tommy</h1>;
+}
+
+export default Tommy;
